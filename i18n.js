d5576c31724dbb7bb0c4f7ac9f7800991f452a8d
Bernd Artmüller <berndartmueller@gmail.com>
2021-08-01 12:22:54 +0200

    without custom page extension

--- i18n.js
+++ i18n.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extensionsRgx: /\.(page|api)\.(tsx|ts|js|mjs|jsx)$/,
+  // extensionsRgx: /\.(page|api)\.(tsx|ts|js|mjs|jsx)$/,
   locales: ['en', 'de'],
   defaultLocale: 'de',
   pages: {
--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const nextTranslate = require('next-translate')
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  pageExtensions: [
-    "page.tsx",
-    "page.ts",
-    "page.jsx",
-    "page.js",
-    "api.ts",
-    "api.js"
-  ]
+  // pageExtensions: [
+  //   "page.tsx",
+  //   "page.ts",
+  //   "page.jsx",
+  //   "page.js",
+  //   "api.ts",
+  //   "api.js"
+  // ]
 };
 
 module.exports = nextTranslate(nextConfig);
